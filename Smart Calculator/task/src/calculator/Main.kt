fc8485a6e11b92fd07387fba4be86f95cbc7376f
Dagim Tesfaye <dagcodes@gmail.com>
2022-08-16 12:53:24 +0300

    Stage 3

--- Smart Calculator/task/src/calculator/Main.kt
+++ Smart Calculator/task/src/calculator/Main.kt
@@ -1,15 +1,25 @@
 package calculator
 
+import kotlin.system.exitProcess
+
 fun main() {
     while(true){
         val input = readln()
-        if (input == "") continue
-        if (input == "/exit") {
-            println("Bye!")
-            return
-        }
+        if (isCommand(input)) continue
         var sum = 0
         for (element in input.split(" ")) sum += element.toInt()
         println(sum)
     }
+}
+fun isCommand(input: String): Boolean {
+    if (input == "") return true
+    if (input == "/help") {
+        println("The program calculates the sum of numbers")
+        return true
+    }
+    if (input == "/exit") {
+        println("Bye!")
+        exitProcess(0)
+    }
+    return false
 }
